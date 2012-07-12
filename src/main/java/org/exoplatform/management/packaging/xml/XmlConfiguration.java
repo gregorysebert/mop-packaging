cda3c0ce2f82dce373cbb09fb4424dc36b4b41a1
gregorysebert <gsebert@exoplatform.com>
2012-07-12 19:10:58 +0200

    Add Xml

---
+++ src/main/java/org/exoplatform/management/packaging/xml/XmlConfiguration.java
@@ -0,0 +1,22 @@
+package org.exoplatform.management.packaging.xml;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gregorysebert
+ * Date: 12/07/12
+ * Time: 19:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class XmlConfiguration {
+
+    public void XmlConfiguration()
+    {
+
+    }
+
+    public void addPortalConfiguration()
+    {
+
+    }
+
+}
